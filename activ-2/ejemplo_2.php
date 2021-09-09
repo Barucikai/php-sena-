d9bb0f889eb57b38c670931a5ea29320b7116582
MurcielagoCyber <braynmos97@gil.com>
2021-09-09 00:04:50 -0500

    todas las activi

---
+++ activ-2/ejemplo_2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Unidad 2 - Ejemplo 3 </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h3>Nombres: </h3>
+        <?php
+            echo "Brayan Stiven <br/>"; //Primer segmento del un programa PHP
+        ?>
+        <h3>Apellidos:</h3>
+        <?php
+            echo "Ramos Casas <br />"; //Segundo segmento del mismo programa
+        ?>
+        <h3>Edad:</h3>
+        <?php
+            echo " 26 Años"; //Tercer segmento del mismo programa
+        ?>
+        <h3>Teléfono:</h3>
+        <?php
+        echo "<b> +57 </b> 305 8340764";
+        ?>
+        <h3>Direción :</h3>
+        <?php
+        echo "<b> Cr. </b> 7 <b> No. </b> 23b - 20 <b> Sur </b>"
+         ?>
+    </body>
+</html>
