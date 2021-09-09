434fb40d536beb8e600d86ebfd76b8a12cf2fa49
MurcielagoCyber <braynmos97@gil.com>
2021-09-09 00:03:32 -0500

    activi-1

---
+++ Activ-1/ejemplo_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> <!-- En los archivos .php el código HTML se escribe normalmente-->
+    <head> <!-- como puedes ver toda la estructura de la página está escrita en HTML-->
+        <title>Ejemplo 1</title>
+        <meta http-equiv="Content-Type"
+              content="text/html; charset=ISO-8859-1" />
+    </head>
+    <body>
+        <p>Mi primer php</p> <!-- Esto está escrito en HTML-->
+        <?php
+            echo "Mi Nombre es: <br> Brayan Ramos"; /* Esto está escrito en PHP*/
+            echo "<br>Mi Numero de Cedula es: <br> 1026582024";
+            echo "<br>Saludos especial al  instructor: <br> Julián Cesar Prado Ramírez,";
+        ?>
+    </body>
+</html>
